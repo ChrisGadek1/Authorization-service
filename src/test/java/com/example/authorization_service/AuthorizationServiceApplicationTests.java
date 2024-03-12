31189ae0711b1f919e54b9137ee8b776fa84e0dd
Krzysztof Gądek <krzysztof.gadek15@gmail.com>
2024-03-13 00:29:02 +0100

    Added working tests

--- src/test/java/com/example/authorization_service/AuthorizationServiceApplicationTests.java
+++ src/test/java/com/example/authorization_service/AuthorizationServiceApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.authorization_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class AuthorizationServiceApplicationTests {
 
+    @Autowired
+    private MockMvc mvc;
+
     @Test
-    void contextLoads() {
+    void contextLoads() throws Exception {
+        mvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello World")));
     }
 
 }
